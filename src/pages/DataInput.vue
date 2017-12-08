fbcefb9e94249a0d18a826fd68f72cbccd99d409
Burak Karakan <burak.karakan@gmail.com>
2017-12-08 16:06:49 +0300

    Created data input page template.

---
+++ src/pages/DataInput.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  sdfsdfd
+</div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'DataInput',
+  data() {
+    return {
+      name: 'Name',
+    };
+  },
+  computed: mapState({
+    items: state => state.sidebar.items,
+  }),
+};
+</script>
+
+<style>
+
+</style>
