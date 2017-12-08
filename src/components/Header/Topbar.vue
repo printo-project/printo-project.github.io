5c765c7e360c780d2e8e2c1baf73f5b98e251ec4
Burak Karakan <burak.karakan@gmail.com>
2017-12-08 17:03:16 +0300

    Added certificate image templates.

--- src/components/Header/Topbar.vue
+++ src/components/Header/Topbar.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="row header">
-    <div class="col-md-3 col-md-offset-1">
+    <div class="col-md-3">
       <image-upload />
     </div>
-    <div class="col-md-3">
-      <line-button v-for="(button, index) in buttons"
-        :key="button.id"
-        :button="button"
-        :index="index" />
-    </div>
-    <div class="col-md-4">
-      <div class="pull-right right-buttons">
+    <div class="col-md-4 offset-md-4">
+      <div class="float-right">
         <remove-buttons :disabled="noItemsExist" />
         <save-button :disabled="noItemsExist" />
       </div>
@@ -21,7 +15,6 @@
 <script>
 import { mapState } from 'vuex';
 import ImageUpload from './ImageUpload';
-import LineButton from './LineButton';
 import RemoveButtons from './RemoveButtons';
 import SaveButton from './SaveButton';
 
@@ -29,19 +22,15 @@ export default {
   name: 'Topbar',
   components: {
     ImageUpload,
-    LineButton,
     RemoveButtons,
     SaveButton,
   },
   computed: {
     noItemsExist() {
-      return !(this.draggableItems.length > 0 ||
-        this.draggableLines.length > 0 ||
-        this.background !== '');
+      return !(this.draggableItems.length > 0 || this.background !== '');
     },
     ...mapState({
       buttons: state => state.topbar.buttons,
-      draggableLines: state => state.draggable.lines,
       draggableItems: state => state.draggable.items,
       background: state => state.page.background,
     }),
@@ -53,10 +42,6 @@ export default {
 .header {
   margin-top: 24px;
   margin-bottom: 24px;
-}
-
-.right-buttons {
-  padding-right: 16px;
 }
 
 .btn-danger {
