cd671d0816bc75476e2c2b77675b60dcc06c5be6
Burak Karakan <burak.karakan@gmail.com>
2017-12-11 15:14:42 +0300

    Added wedding card title test.

--- test/unit/specs/Index.spec.js
+++ test/unit/specs/Index.spec.js
@@ -8,4 +8,10 @@ describe('Index.vue', () => {
 
     expect(vm.$el.querySelector('h1.display-3').textContent).to.equal('Design & Print');
   });
+  it('Invitation card is there', () => {
+    const Constructor = Vue.extend(Index);
+    const vm = new Constructor().$mount();
+
+    expect(vm.$el.querySelector('.card .col-md-8 h5').textContent).to.equal('Wedding Invitation');
+  });
 });
